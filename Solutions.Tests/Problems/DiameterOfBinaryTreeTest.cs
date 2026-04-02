da865778bb6b88eaba2453dc6edbb643264fcfe0
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2026-04-02 00:57:14 -0300

    [feat] diameter of binary tree skeleton

---
+++ Solutions.Tests/Problems/DiameterOfBinaryTreeTest.cs
@@ -0,0 +1,37 @@
+﻿using LeetCode.Solutions.Models;
+using Solutions.Problems;
+
+namespace Solutions.Tests.Problems;
+
+public class DiameterOfBinaryTreeTest
+{
+    public DiameterOfBinaryTreeSolution Solution { get; set; } = new();
+
+    [Fact]
+    public void Case1()
+    {
+        // Arrange
+        int[] nums = [1, 2, 3, 4, 5];
+        TreeNode? treeNode = TreeNode.FromArray(nums);
+
+        // Act
+        int actual = Solution.DiameterOfBinaryTree(treeNode);
+
+        // Assert
+        Assert.Equal(3, actual);
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        // Arrange
+        int[] nums = [1, 2];
+        TreeNode? treeNode = TreeNode.FromArray(nums);
+
+        // Act
+        int actual = Solution.DiameterOfBinaryTree(treeNode);
+
+        // Assert
+        Assert.Equal(1, actual);
+    }
+}
---
+++ Solutions/Problems/DiameterOfBinaryTree.cs
@@ -0,0 +1,11 @@
+﻿using LeetCode.Solutions.Models;
+
+namespace Solutions.Problems;
+
+public class DiameterOfBinaryTreeSolution
+{
+    public int DiameterOfBinaryTree(TreeNode? root)
+    {
+        return 0;
+    }
+}
