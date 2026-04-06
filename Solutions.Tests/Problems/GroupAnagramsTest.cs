e1f5d9469935d0138a552ab0c3f224db6570ed68
Gabriel Albuquerque da Rosa <gabriel.rosa@effectiveerp.com.br>
2026-04-06 01:34:30 -0300

    [feat] 49. group anagrams

---
+++ Solutions.Tests/Problems/GroupAnagramsTest.cs
@@ -0,0 +1,58 @@
+﻿using Solutions.Problems;
+
+namespace Solutions.Tests.Problems;
+
+public class GroupAnagramsTest
+{
+    public GroupAnagramsSolution Solution { get; set; } = new();
+
+    [Fact]
+    public void Case1()
+    {
+        var input = new[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+        var expected = new List<List<string>>
+        {
+            new() { "bat" },
+            new() { "nat", "tan" },
+            new() { "ate", "eat", "tea" }
+        };
+
+        var result = Solution.GroupAnagramsSortLinq(input);
+
+        Assert.Equal(expected.Count, result.Count);
+        foreach (var group in expected)
+            Assert.Contains(result, r => r.Count == group.Count && r.All(group.Contains));
+    }
+
+    [Fact]
+    public void Case2()
+    {
+        var input = new[] { "" };
+        var expected = new List<List<string>>
+        {
+            new() { "" }
+        };
+
+        var result = Solution.GroupAnagramsSortLinq(input);
+
+        Assert.Equal(expected.Count, result.Count);
+        foreach (var group in expected)
+            Assert.Contains(result, r => r.Count == group.Count && r.All(group.Contains));
+    }
+
+    [Fact]
+    public void Case3()
+    {
+        var input = new[] { "a" };
+        var expected = new List<List<string>>
+        {
+            new() { "a" }
+        };
+
+        var result = Solution.GroupAnagramsSortLinq(input);
+
+        Assert.Equal(expected.Count, result.Count);
+        foreach (var group in expected)
+            Assert.Contains(result, r => r.Count == group.Count && r.All(group.Contains));
+    }
+}
---
+++ Solutions/Problems/GroupAnagrams.cs
@@ -0,0 +1,50 @@
+﻿namespace Solutions.Problems;
+
+public class GroupAnagramsSolution
+{
+    public List<List<string>> GroupAnagrams(string[] strs)
+    {
+        var hashMap = new Dictionary<string, List<string>>();
+
+        foreach (var str in strs)
+        {
+            var letters = string.Concat(str.OrderBy(s => s));
+
+            if (hashMap.TryGetValue(letters, out var strings))
+                strings.Add(str);
+            else
+                hashMap[letters] = [str];
+        }
+
+        return [.. hashMap.Values];
+    }
+
+    public IList<IList<string>> GroupAnagramsSortLinq(string[] strs)
+    {
+        var groups = strs.GroupBy(s => new string([.. s.OrderBy(c => c)]))
+                         .ToDictionary(g => g.Key, g => g.ToList());
+
+        return [.. groups.Values];
+    }
+
+    public List<List<string>> GroupAnagramsHashMap(string[] strs)
+    {
+        var hashMap = new Dictionary<string, List<string>>();
+
+        foreach (var str in strs)
+        {
+            var count = new int[26];
+            foreach (var c in str)
+                count[c - 'a']++;
+
+            var key = string.Join(string.Empty, count);
+            if (hashMap.TryGetValue(key, out var strings))
+                strings.Add(str);
+            else
+                hashMap[key] = [str];
+        }
+
+        return [.. hashMap.Values];
+    }
+
+}
