f5e79ac8f49b53325ad8f8b22c3779820b90e4c8
Gabriel A da Rosa <gabriel.rosa@effectiveerp.com.br>
2026-04-07 13:27:47 -0300

    [feat] brute force and iterative solutions for diameter of binary tree

--- Solutions/Problems/DiameterOfBinaryTree.cs
+++ Solutions/Problems/DiameterOfBinaryTree.cs
@@ -4,6 +4,29 @@ namespace Solutions.Problems;
 
 public class DiameterOfBinaryTreeSolution
 {
+    public int DiameterOfBinaryTreeBruteForce(TreeNode? root)
+    {
+        static int Depth(TreeNode? node)
+        {
+            if (node == null)
+                return 0;
+
+            return 1 + Math.Max(Depth(node.left), Depth(node.right));
+        }
+
+        if (root == null)
+            return 0;
+
+        int leftHeight = Depth(root.left);
+        int rightHeight = Depth(root.right);
+
+        int diameter = leftHeight + rightHeight;
+        int sub = Math.Max(DiameterOfBinaryTreeBruteForce(root.left), DiameterOfBinaryTreeBruteForce(root.right));
+
+        return Math.Max(diameter, sub);
+    }
+
+    #region ClassMemberDFS
     public int result = 0;
 
     public int DiameterOfBinaryTreeClassMemberDFS(TreeNode? root)
@@ -23,6 +46,7 @@ public class DiameterOfBinaryTreeSolution
         Depth(root);
         return result;
     }
+    #endregion
 
     public int DiameterOfBinaryTreeDFS(TreeNode? root)
     {
@@ -41,5 +65,49 @@ public class DiameterOfBinaryTreeSolution
         int result = 0;
         Depth(root, ref result);
         return result;
+    }
+
+    public int DiameterOfBinaryTreeIterativeDFS(TreeNode? root)
+    {
+        if (root == null)
+            return 0;
+
+        var hashMap = new Dictionary<TreeNode, (int height, int diameter)>();
+        Stack<TreeNode> stack = new();
+        stack.Push(root);
+
+        while (stack.Count > 0)
+        {
+            TreeNode node = stack.Peek();
+
+            if (node.left != null && !hashMap.ContainsKey(node.left))
+            {
+                stack.Push(node.left);
+                continue;
+            }
+
+            if (node.right != null && !hashMap.ContainsKey(node.right))
+            {
+                stack.Push(node.right);
+                continue;
+            }
+
+            node = stack.Pop();
+
+            (int leftHeight, int leftDiameter) = (0, 0);
+            if (node.left != null && hashMap.TryGetValue(node.left, out var valueLeft))
+                (leftHeight, leftDiameter) = valueLeft;
+
+            (int rightHeight, int rightDiameter) = (0, 0);
+            if (node.right != null && hashMap.TryGetValue(node.right, out var valueRight))
+                (rightHeight, rightDiameter) = valueRight;
+
+            int diameter = Math.Max(leftHeight + rightHeight, Math.Max(leftDiameter, rightDiameter));
+            int height = 1 + Math.Max(leftHeight, rightHeight);
+
+            hashMap[node] = (height, diameter);
+        }
+
+        return hashMap[root].diameter;
     }
 }
